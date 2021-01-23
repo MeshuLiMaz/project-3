2aca4a264b94440febee05f1e810057605447171
Michelle A Limon <mlimon0789@gmail.com>
2021-01-22 19:20:10 -0600

    adding new file

---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function Nav(props) {
+    return (
+        <div>
+            this is my Navbar
+        </div>
+    );
+}
+
+export default Nav;
