4fb69740e83d8534821d65ba228ed54a6c0482e1
Michelle A Limon <mlimon0789@gmail.com>
2021-01-22 19:13:37 -0600

    cleaning it up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
 
-      <div className={'p-10 bg-gray-400'}>
-        yay
-        <button type={'primary'}>Yay</button>
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
+    
   );
 }
 
 export default App;
 
 
+
+//<div className="App">
+    
+  //  <header className="App-header">
+    //  <img src={logo} className="App-logo" alt="logo" />
+      //<p>
+        //Edit <code>src/App.js</code> and save to reload.
+      //</p>
+      //<a
+        //className="App-link"
+        //href="https://reactjs.org"
+        //target="_blank"
+        //rel="noopener noreferrer"
+      //>
+        //Learn React
+      //</a>
+    //</header>
+  //</div>
