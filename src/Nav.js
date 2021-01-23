89cb0666fbb874d1cb80eb5d6d6d6a6e370ce69c
Michelle A Limon <mlimon0789@gmail.com>
2021-01-22 19:34:47 -0600

    Messing around

--- src/Nav.js
+++ src/Nav.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 function Nav(props) {
     return (
-        <div classname={'p-10'}>
-            this is my Navbar
+        <div classname={'p-5'}>
+            this is my Nav bar
         </div>
     );
 }
