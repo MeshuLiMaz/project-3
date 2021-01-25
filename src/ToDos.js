04eee7aac37847ab18b3de538665f55c03a23018
Michelle A Limon <mlimon0789@gmail.com>
2021-01-24 23:48:55 -0600

    adding more detail for To-do's page

--- src/ToDos.js
+++ src/ToDos.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react';
+import {Button} from "antd";
 
 function ToDos(props) {
     return (
-        <div className={'p-5 border m-5 md:m-12 lg:m-20'}>
-            ToDos page
+        <div className={'p-5 border m-5 md:m-12 lg:m-20 flex justify-center items-center'}>
+            
+            <input type="text" className={'border p-2 mr-10 rounded w-full'} placeholder={'Task...'} />
+
+
+            <Button type={'primary rounded'}>Add</Button>
+
         </div>
     );
 }
