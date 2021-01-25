5022df222795087ea293fd32e6bcb0249c716532
Michelle A Limon <mlimon0789@gmail.com>
2021-01-24 23:32:38 -0600

    messing with margins

--- src/ToDos.js
+++ src/ToDos.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 function ToDos(props) {
     return (
-        <div>
-            ToDos Page
+        <div className={'p-5 border m-5 md:m-12 lg:m-20'}>
+            ToDos page
         </div>
     );
 }
