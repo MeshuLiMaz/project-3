7507ba7ae33e1bb592aa518734fa1e570c72ac95
Michelle A Limon <mlimon0789@gmail.com>
2021-01-25 18:03:54 -0600

    Adding more detail

--- src/ToDos.js
+++ src/ToDos.js
@@ -3,12 +3,32 @@ import {Button} from "antd";
 
 function ToDos(props) {
     return (
-        <div className={'p-5 border m-5 md:m-12 lg:m-20 flex justify-center items-center'}>
+        <div className={'p-5 border m-5 md:m-12 lg:m-20'}>
             
+            <div className={'flex justify-center items-center'}>
             <input type="text" className={'border p-2 mr-5 rounded w-full'} placeholder={'Task...'} />
 
-
             <Button type={'primary'}>Add</Button>
+
+            </div>
+
+            <div className={'bg-gray-100 mt-5 p-5'}>
+                <ul>
+                    <li className={'p-1 border-b-2'}>
+                        <input type="checkbox" className={'mr-2'}/>
+                        Do Laundry
+                    </li>
+                    <li className={'p-1 border-b-2'}>
+                        <input type="checkbox" className={'mr-2'}/>
+                        Schedule Doc appt
+                    </li>
+                    <li className={'p-1 border-b-2'}>
+                        <input type="checkbox" className={'mr-2'}/>
+                        Schedule Wedding food tasting
+                    </li>
+
+                </ul>
+            </div>
 
         </div>
     );
